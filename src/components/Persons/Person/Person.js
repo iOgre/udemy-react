0f66d1026f8cf8295db20c80c0b05899e734a6b1
iOgre <igorvadimovich@outlook.com>
2018-07-07 22:50:30 +0500

    65 finished (Using radium with media queries)

--- src/components/Persons/Person/Person.js
+++ src/components/Persons/Person/Person.js
@@ -1,12 +1,13 @@
 import React from 'react';
-import './Person.css'
+import './Person.css';
+import Radium from 'radium';
 
 const person = (props) => {
   const style = {
-    '@media screen and  (min-width: 500px)': {
+    '@media (min-width: 500px)': {
       width: '450px',
-      backgroundColor: 'black'
-    }
+      }
+    
   };
   return (<div className="Person" style={style}>
     <p onClick={props.click}>I am {props.name} and I am {props.age} years old</p>
@@ -15,4 +16,4 @@ const person = (props) => {
   </div>)
 };
 
-export default person;
+export default Radium(person);
